fc9d20a12d7b78961cf67897c769e2259d8a96bc
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-05 00:24:17 +0000

    add routing configuration with root and bye paths

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter();
+const router = createBrowserRouter([
+  {
+    element: <p>Hi !!! 🐸</p>,
+    path: "/", // This is the root path
+  },
+  {
+    element: <i>Bye !!! 🐨</i>,
+    path: "/bye", // This is the /bye path
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
