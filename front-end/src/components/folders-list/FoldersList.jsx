625c12b89894e738db222b0d9d315cf2cb3e17ee
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-08 01:23:32 +0000

    replace anchor tags with Link components for folder navigation

--- front-end/src/components/folders-list/FoldersList.jsx
+++ front-end/src/components/folders-list/FoldersList.jsx
@@ -5,6 +5,7 @@ import { Folder } from "../folder/Folder";
 import { Title } from "../title/Title";
 import { TopBar } from "../top-bar/TopBar";
 import { AddNewButton } from "../add-new-button/AddNewButton";
+import { Link } from "react-router-dom";
 
 const Folders = ({ children }) => (
   <div className={styles["folders-column"]}>{children}</div>
@@ -41,9 +42,9 @@ const FoldersList = () => {
       <Title>Folders</Title>
       <UserCreatedFolders>
         {folders.map((folder) => (
-          <a key={folder.id} href={`/notes/${folder.id}`}>
+          <Link key={folder.id} to={`/notes/${folder.id}`}>
             <Folder>{folder.name}</Folder>
-          </a>
+          </Link>
         ))}
       </UserCreatedFolders>
       {/* <Folder icon="archive">Archiwum</Folder> */}
