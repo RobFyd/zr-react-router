84931af315bb49e2f79ce69ed20250afbb6d68da
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-12 00:07:52 +0000

    feat: add nested routing for notes and individual note fetching in main router

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
-import NotesList from "./components/notes-list/NotesList";
+import { NotesList } from "./components/notes-list/NotesList";
+import { Note } from "./components/note/Note";
 
 const router = createBrowserRouter([
   {
@@ -13,13 +14,22 @@ const router = createBrowserRouter([
     },
     children: [
       {
-        element: <NotesList />,
         path: "/notes/:folderId",
+        element: <NotesList />,
         loader: ({ params }) => {
           return fetch(
             `http://localhost:3000/notes?folderId=${params.folderId}`
           );
         },
+        children: [
+          {
+            path: "/notes/:folderId/note/:noteId",
+            element: <Note />,
+            loader: ({ params }) => {
+              return fetch(`http://localhost:3000/notes/${params.noteId}`);
+            },
+          },
+        ],
       },
     ],
   },
