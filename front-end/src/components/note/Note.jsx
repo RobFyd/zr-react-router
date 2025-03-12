da49390bf8cc7129ca2679f8ba914eb51bc3adc7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-12 00:17:06 +0000

    feat: implement note fetching and editing using useLoaderData in Note component

--- front-end/src/components/note/Note.jsx
+++ front-end/src/components/note/Note.jsx
@@ -1,26 +1,26 @@
 import RemoveIcon from "../../assets/remove.svg";
 import styles from "./Note.module.css";
 import { TopBar } from "../top-bar/TopBar";
+import { Form, useLoaderData } from "react-router-dom";
 
 const NoteEditor = ({ children }) => (
-    <div className={styles["note-editor"]}>{children}</div>
+  <div className={styles["note-editor"]}>{children}</div>
 );
 
-const Note = () => {
-    return (
-        <div className={styles.container}>
-            <TopBar>
-                <button className={styles.button}>
-                    <img className={styles.image} src={RemoveIcon} />
-                </button>
-            </TopBar>
+export function Note() {
+  const note = useLoaderData();
+  return (
+    <div className={styles.container}>
+      <TopBar>
+        <button className={styles.button}>
+          <img className={styles.image} src={RemoveIcon} />
+        </button>
+      </TopBar>
 
-            <NoteEditor>
-                <input type="text" />
-                <textarea />
-            </NoteEditor>
-        </div>
-    );
-};
-
-export { Note };
+      <NoteEditor key={note.id}>
+        <input type="text" defaultValue={note.title} />
+        <textarea defaultValue={note.body} />
+      </NoteEditor>
+    </div>
+  );
+}
