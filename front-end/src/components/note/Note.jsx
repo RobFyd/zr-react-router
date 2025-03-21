d3e1eebdf665be24b391137e7e52eade5dd0a2ab
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-21 00:43:31 +0000

    app updated

--- front-end/src/components/note/Note.jsx
+++ front-end/src/components/note/Note.jsx
@@ -1,57 +1,64 @@
 import RemoveIcon from "../../assets/remove.svg";
 import styles from "./Note.module.css";
 import { TopBar } from "../top-bar/TopBar";
-import { Form, useLoaderData, useSubmit, redirect } from "react-router-dom";
+import { useLoaderData, Form, useSubmit, redirect } from "react-router-dom";
 
 const NoteEditor = ({ children }) => (
-  <div className={styles["note-editor"]}>{children}</div>
+    <div className={styles["note-editor"]}>{children}</div>
 );
 
+export async function deleteNote({ params }) {
+    return fetch(`http://localhost:3000/notes/${params.noteId}`, {
+        method: "DELETE",
+    }).then(() => {
+        return redirect(`/notes/${params.folderId}`);
+    });
+}
+
 export async function updateNote({ request, params }) {
-  const data = await request.formData();
-  const title = data.get("title");
-  const body = data.get("body");
-  return fetch(`http://localhost:3000/notes/${params.noteId}`, {
-    method: "PATCH",
-    body: JSON.stringify({ title, body }),
-    headers: {
-      "Content-type": "application/json",
-    },
-  });
+    const data = await request.formData();
+
+    const title = data.get("title");
+    const body = data.get("body");
+
+    return fetch(`http://localhost:3000/notes/${params.noteId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+            title,
+            body,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 }
 
-export function deleteNote({ params }) {
-  return fetch(`http://localhost:3000/notes/${params.noteId}`, {
-    method: "DELETE",
-  }).then(() => {
-    return redirect(`/notes/${params.folderId}`);
-  });
-}
+const Note = () => {
+    const note = useLoaderData();
+    const submit = useSubmit();
 
-export function Note() {
-  const note = useLoaderData();
-  const submit = useSubmit();
-  return (
-    <div className={styles.container}>
-      <TopBar>
-        <Form method="DELETE" action="delete">
-          <button className={styles.button}>
-            <img className={styles.image} src={RemoveIcon} />
-          </button>
-        </Form>
-      </TopBar>
+    return (
+        <div className={styles.container}>
+            <TopBar>
+                <Form method="DELETE" action="delete">
+                    <button className={styles.button}>
+                        <img className={styles.image} src={RemoveIcon} />
+                    </button>
+                </Form>
+            </TopBar>
+            <Form
+                method="PATCH"
+                onChange={(event) => {
+                    submit(event.currentTarget);
+                }}
+            >
+                <NoteEditor key={note.id}>
+                    <input type="text" defaultValue={note.title} name="title" />
+                    <textarea defaultValue={note.body} name="body" />
+                </NoteEditor>
+            </Form>
+        </div>
+    );
+};
 
-      <Form
-        method="PATCH"
-        onChange={(event) => {
-          submit(event.currentTarget);
-        }}
-      >
-        <NoteEditor key={note.id}>
-          <input type="text" name="title" defaultValue={note.title} />
-          <textarea name="body" defaultValue={note.body} />
-        </NoteEditor>
-      </Form>
-    </div>
-  );
-}
+export { Note };
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,73 +5,66 @@ import { App } from "./App";
 import NotesList from "./components/notes-list/NotesList";
 import { deleteNote, Note } from "./components/note/Note";
 import { createFolder } from "./components/folders-list/FoldersList";
-import { createNote } from "./components/notes-list/NotesList";
+import { createNewNote } from "./components/notes-list/NotesList";
 import { updateNote } from "./components/note/Note";
 
-// const NotFound = () => (
-//   <h1 style={{ textAlign: "center", marginTop: "50px" }}>404 - Not Found</h1>
-// );
-
 const router = createBrowserRouter([
-  {
-    element: <App />,
-    path: "/", // This is the root path
-    action: createFolder,
-    loader: () => {
-      return fetch("http://localhost:3000/folders");
-    },
-    shouldRevalidate: ({ formAction }) => {
-      if (formAction === "/") {
-        return true;
-      } else {
-        return false;
-      }
-    },
-
-    children: [
-      {
-        path: "/notes/:folderId",
-        element: <NotesList />,
-        action: createNote,
-        loader: ({ params }) => {
-          return fetch(
-            `http://localhost:3000/notes?folderId=${params.folderId}`
-          );
+    {
+        element: <App />,
+        path: "/",
+        action: createFolder,
+        loader: () => {
+            return fetch("http://localhost:3000/folders");
+        },
+        shouldRevalidate: ({ formAction }) => {
+            if (formAction === "/") {
+                return true;
+            } else {
+                return false;
+            }
         },
         children: [
-          {
-            path: "note/:noteId",
-            element: <Note />,
-            action: updateNote,
-            loader: ({ params }) => {
-              return fetch(`http://localhost:3000/notes/${params.noteId}`);
+            {
+                path: "notes/:folderId",
+                element: <NotesList />,
+                action: createNewNote,
+                loader: ({ params }) => {
+                    return fetch(
+                        `http://localhost:3000/notes?folderId=${params.folderId}`
+                    );
+                },
+                children: [
+                    {
+                        path: `note/:noteId`,
+                        element: <Note />,
+                        action: updateNote,
+                        loader: ({ params }) => {
+                            return fetch(
+                                `http://localhost:3000/notes/${params.noteId}`
+                            );
+                        },
+                        shouldRevalidate: ({ formAction }) => {
+                            if (formAction) {
+                                return false;
+                            } else {
+                                return true;
+                            }
+                        },
+                        children: [
+                            {
+                                path: "delete",
+                                action: deleteNote,
+                            },
+                        ],
+                    },
+                ],
             },
-            shouldRevalidate: ({ formAction }) => {
-              if (formAction) {
-                return false;
-              } else {
-                return true;
-              }
-            },
-            children: [
-              {
-                path: "delete",
-                action: deleteNote,
-              },
-            ],
-          },
         ],
-      },
-    ],
-  },
-  // {
-  //   path: "*",
-  //   element: <NotFound />,
-  // },
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
