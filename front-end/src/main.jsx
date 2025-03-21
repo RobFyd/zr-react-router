3e115654879ed7a748c90df8ccc68c957250a214
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-21 23:58:48 +0000

    add NotFound component as error element in main router configuration

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,64 +7,64 @@ import { deleteNote, Note } from "./components/note/Note";
 import { createFolder } from "./components/folders-list/FoldersList";
 import { createNewNote } from "./components/notes-list/NotesList";
 import { updateNote } from "./components/note/Note";
+import { NotFound } from "./components/not-found/NotFound";
 
 const router = createBrowserRouter([
-    {
-        element: <App />,
-        path: "/",
-        action: createFolder,
-        loader: () => {
-            return fetch("http://localhost:3000/folders");
-        },
-        shouldRevalidate: ({ formAction }) => {
-            if (formAction === "/") {
-                return true;
-            } else {
-                return false;
-            }
+  {
+    element: <App />,
+    path: "/",
+    errorElement: <NotFound />,
+    action: createFolder,
+    loader: () => {
+      return fetch("http://localhost:3000/folders");
+    },
+    shouldRevalidate: ({ formAction }) => {
+      if (formAction === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    children: [
+      {
+        path: "notes/:folderId",
+        element: <NotesList />,
+        action: createNewNote,
+        loader: ({ params }) => {
+          return fetch(
+            `http://localhost:3000/notes?folderId=${params.folderId}`
+          );
         },
         children: [
-            {
-                path: "notes/:folderId",
-                element: <NotesList />,
-                action: createNewNote,
-                loader: ({ params }) => {
-                    return fetch(
-                        `http://localhost:3000/notes?folderId=${params.folderId}`
-                    );
-                },
-                children: [
-                    {
-                        path: `note/:noteId`,
-                        element: <Note />,
-                        action: updateNote,
-                        loader: ({ params }) => {
-                            return fetch(
-                                `http://localhost:3000/notes/${params.noteId}`
-                            );
-                        },
-                        shouldRevalidate: ({ formAction }) => {
-                            if (formAction) {
-                                return false;
-                            } else {
-                                return true;
-                            }
-                        },
-                        children: [
-                            {
-                                path: "delete",
-                                action: deleteNote,
-                            },
-                        ],
-                    },
-                ],
+          {
+            path: `note/:noteId`,
+            element: <Note />,
+            action: updateNote,
+            loader: ({ params }) => {
+              return fetch(`http://localhost:3000/notes/${params.noteId}`);
             },
+            shouldRevalidate: ({ formAction }) => {
+              if (formAction) {
+                return false;
+              } else {
+                return true;
+              }
+            },
+            children: [
+              {
+                path: "delete",
+                action: deleteNote,
+              },
+            ],
+          },
         ],
-    },
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
